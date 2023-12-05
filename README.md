a44f82a3dec4f703b74ba4e5dc70c9f6ab4ae9bc
Paul J. Davis <paul.davis@tiledb.com>
2023-12-05 13:55:31 -0600

    Initial Import

---
+++ README.md
@@ -0,0 +1,4 @@
+TileDB Log Testing
+===
+
+A small test repository for logging API calls.
---
+++ scripts/add-dependency.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import sys
+
+DEP_NAME = "tiledb_capi_logging"
+
+def main():
+  if len(sys.argv) != 2:
+    print("usage: {} vcpkg.json".format(sys.argv[0]))
+    exit(1)
+
+  with open(sys.argv[1]) as handle:
+    data = json.load(handle)
+
+  if DEP_NAME in data["dependencies"]:
+    return
+
+  data["dependencies"].append(DEP_NAME)
+  with open(sys.argv[1], "w") as handle:
+    json.dump(data, handle, indent = 4, sort_keys = True)
+
+if __name__ == "__main__":
+  main()
