3ab1d136961a7d76eecadc1653c2f99422453a6c
Paul J. Davis <paul.davis@tiledb.com>
2023-12-06 15:23:36 -0600

    Link tdb_capi_shared to tiledb_shared as well

--- cmake/TileDBExtra.cmake
+++ cmake/TileDBExtra.cmake
@@ -2,7 +2,15 @@
 # to setup custom dependencies.
 
 find_package(tdb_capi_logging REQUIRED)
+
+message(STATUS "Adding tdb_capi_logging dependencies.")
+
 target_link_libraries(
   baseline
   INTERFACE
+    tdb_capi_logging::tdb_capi_logging)
+
+target_link_libraries(
+  tiledb_shared
+  PUBLIC
     tdb_capi_logging::tdb_capi_logging)
